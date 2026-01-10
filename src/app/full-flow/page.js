13bd8ac73cd125452f06f9cf3070bdccc3ecf5bb
suryakumar2 <surya@pointblank.co.in>
2026-01-11 02:11:13 +0530

    Restructured the page flow

--- src/app/full-flow/page.js
+++ src/app/full-flow/page.js
@@ -3,24 +3,24 @@
 // Import all section components
 import HomepageSection from "../components/HomepageSection";
 import BaseCampGenesisSection from "../components/BaseCampGenesisSection";
-import IntoTheWildSection from "../components/IntoTheWildSection";
-import FootprintsSecuredSection from "../components/FootprintsSecuredSection";
-import TheNextExpeditionSection from "../components/TheNextExpeditionSection";
 import GearUnlockedSection from "../components/GearUnlockedSection";
+import IntoTheWildSection from "../components/IntoTheWildSection";
 import AcrossTheTrailSection from "../components/AcrossTheTrailSection";
+import FootprintsSecuredSection from "../components/FootprintsSecuredSection";
 import VoiceFromCommanderSection from "../components/VoiceFromCommanderSection";
+import TheNextExpeditionSection from "../components/TheNextExpeditionSection";
 
 export default function FullFlow() {
   return (
     <div className="full-flow-container">
       <HomepageSection useScrollLinks={true} />
       <BaseCampGenesisSection />
-      <IntoTheWildSection />
-      <FootprintsSecuredSection />
-      <TheNextExpeditionSection />
       <GearUnlockedSection />
+      <IntoTheWildSection />
       <AcrossTheTrailSection />
+      <FootprintsSecuredSection />
       <VoiceFromCommanderSection />
+      <TheNextExpeditionSection />
     </div>
   );
 }
