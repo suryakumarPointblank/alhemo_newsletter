26d84cba9689be37b56ce14cbcf2661e5834706f
suryakumar2 <surya@pointblank.co.in>
2026-01-22 21:48:56 +0530

    Updated the flow to a single page one

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,25 @@
 "use client";
 
 import HomepageSection from "./components/HomepageSection";
+import BaseCampGenesisSection from "./components/BaseCampGenesisSection";
+import GearUnlockedSection from "./components/GearUnlockedSection";
+import IntoTheWildSection from "./components/IntoTheWildSection";
+import AcrossTheTrailSection from "./components/AcrossTheTrailSection";
+import FootprintsSecuredSection from "./components/FootprintsSecuredSection";
+import VoiceFromCommanderSection from "./components/VoiceFromCommanderSection";
+import TheNextExpeditionSection from "./components/TheNextExpeditionSection";
 
 export default function Home() {
   return (
-    <div className="page-container">
-      <HomepageSection useScrollLinks={false} />
+    <div className="full-flow-container">
+      <HomepageSection useScrollLinks={true} />
+      <BaseCampGenesisSection />
+      <GearUnlockedSection />
+      <IntoTheWildSection />
+      <AcrossTheTrailSection />
+      <FootprintsSecuredSection />
+      <VoiceFromCommanderSection />
+      <TheNextExpeditionSection />
     </div>
   );
 }
